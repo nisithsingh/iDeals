a6614df86c16d4ac92b9989bf8ace1bcaa313877
student <student@CR33S22I.local>
2014-05-06 02:48:29 +0800

    detail

--- iDeals/DetailViewController.h
+++ iDeals/DetailViewController.h
@@ -11,8 +11,14 @@
 @interface DetailViewController : UIViewController
 
 @property (strong, nonatomic) id detailItem;
+@property (strong, nonatomic) IBOutlet UIView *promotionImage;
+@property (weak, nonatomic) IBOutlet UILabel *discountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *actualPriceLabel;
 
-@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *DiscountedPriceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *youSaveLabel;
+@property (weak, nonatomic) IBOutlet UILabel *startDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *EndDateLabel;
 
 
 @end
